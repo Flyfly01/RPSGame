617ad8f03b089442b67af9d09d82738f5c19d1fe
Flyfly01 <shannonimbert22@gmail.com>
2024-09-19 11:19:28 -0400

    Complete STEP 2 with logic for Human Choice

--- index.js
+++ index.js
@@ -8,17 +8,16 @@ function getComputerChoice() {
   return choice;
 
 }
-  
-getComputerChoice();
 
 // STEP 2: Write the logic to get the human choice
 
-//const computerChoice = choices[randomChoice];
-  //return computerChoice;
-/*
-  function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const randChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(randChoice);
-  
-*/
+function getHumanChoice() {
+  let humanChoice = prompt('Do you choose Rock,Paper or Scissors?');
+  if (humanChoice != 'Rock', 'Paper', 'Scissors'); {
+    alert ('Invalid choice! Please choose Rock, Paper or Scissors');
+    humanChoice = prompt('Do you choose Rock, Paper or Scissors?');
+  }
+}
+
+getHumanChoice();
+
