8b34ea6a97634279dbe77e53b06734c9da70044a
Flyfly01 <shannonimbert22@gmail.com>
2024-09-19 13:27:48 -0400

    Complete STEP 4 of writing logic to play a single round

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function getComputerChoice() {
 // STEP 2: Write the logic to get the human choice
 
 function getHumanChoice() {
-  let humanChoice = prompt('Do you choose Rock,Paper or Scissors?');
+  let humanChoice = prompt('Do you choose Rock,Paper or Scissors?').toLowerCase();
   if (humanChoice != 'Rock', 'Paper', 'Scissors'); {
     alert ('Invalid choice! Please choose Rock, Paper or Scissors');
     humanChoice = prompt('Do you choose Rock, Paper or Scissors?');
@@ -24,3 +24,19 @@ function getHumanChoice() {
 
 // STEP 3: Declare the players score variables and initialize them with a value of 0
 // These are global variables so I am placing them at the top 
+
+// STEP 4: Write the logic to play a single round
+
+function playRound (humanChoice, computerChoice) {
+  if (humanChoice === 'rock' && computerChoice === 'scissors' || 
+      humanChoice === 'paper' && computerChoice === 'rock' ||
+      humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log ('Congratulations! You win!');
+        humanScore++
+      } else if (humanChoice === computerChoice) {
+        console.log ("It's a tie");
+      } else {
+        console.log ('You lose!');
+        computerScore++
+      }
+}
