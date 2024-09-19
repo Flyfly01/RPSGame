179a9511e7fa0847bbe2302f32fec08d0be753f4
Flyfly01 <shannonimbert22@gmail.com>
2024-09-19 11:02:04 -0400

    Redo STEP 1 in simpler terms

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // STEP 1: Write the logic to get the computer choice
 
-console.log('Hello');
+console.log('Hello Shannon');
 
 function getComputerChoice() {
-  const choices = ['Rock', 'Paper', 'Scissors'];
-  const randChoice = choices[Math.floor(Math.random()*3)];
-  const computerChoice = choices[randomChoice];
-  return computerChoice;
+  const choice = ['Rock', 'Paper', 'Scissors'];
+  const randChoice = choice[Math.floor(Math.random() * choice.length)];
+  return choice;
 
-  console.log(computerChoice)
 }
+  
+getComputerChoice();
 
+// STEP 2: Write the logic to get the human choice
+
+//const computerChoice = choices[randomChoice];
+  //return computerChoice;
+/*
+  function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.log(randChoice);
+  
+*/
